64ec1c8f427f60f05e64fbcdaaaff83f46d2166f
Montywilliams <3650@holbertonschool.com>
2023-11-02 18:36:57 -0600

    remove background; enable jit mode; set padding for container

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,16 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  mode: "jit",
+
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      padding: {
+        DEFAULT: '15px',
       },
+    },
+    extend: {
+      backgroundImage: {},
     },
   },
   plugins: [],
