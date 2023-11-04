f78141a02fac1dcdd20bcb82adf4b9f6942122f1
Montywilliams <3650@holbertonschool.com>
2023-11-03 23:57:40 -0600

    import event box

--- src/app/components/events/Events.jsx
+++ src/app/components/events/Events.jsx
@@ -1,3 +1,4 @@
+import EventBox from "./EventBox";
 
 const getEvents = async () => {
   const res = await fetch('http://localhost:4000/events');
@@ -11,7 +12,7 @@ const Events = async () => {
     <div>
       <section className="section" id="tours">
         <div className="container mx-auto">
-
+          <EventBox />
         </div>
       </section>
     </div>
