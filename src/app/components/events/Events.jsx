529ee82fe60d62d1785db0a632c83e8ee56ba3fa
Montywilliams <3650@holbertonschool.com>
2023-11-03 23:44:22 -0600

    correct syntax

---
+++ src/app/components/events/Events.jsx
@@ -0,0 +1,17 @@
+
+const getEvents = async () => {
+  const res = await fetch('http://localhost:4000/events');
+  return res.json();
+}
+
+const Events = async () => {
+  const events = await getEvents();
+  console.log(events);
+  return (
+    <div>
+      Events
+    </div>
+  )
+}
+
+export default Events
