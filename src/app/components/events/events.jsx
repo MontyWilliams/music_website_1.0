8486fb9bee9c822ba3927ad90f4611f9b434ffca
Montywilliams <3650@holbertonschool.com>
2023-11-03 23:13:58 -0600

    create async fetch to get events from local source

--- src/app/components/events/events.jsx
+++ src/app/components/events/events.jsx
@@ -1,7 +1,14 @@
+
+const getEvents = async () => {
+  const res = await fetch('http://localhost:4000/envents');
+  return res.json();
+}
+
 function events() {
   return (
     <div>
-      
+      const events = await getEvents();
+      console.log(events);
     </div>
   )
 }
