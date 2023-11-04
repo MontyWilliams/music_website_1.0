c680db14ad843a0563daf8859be67111c18f8c9b
Montywilliams <3650@holbertonschool.com>
2023-11-04 00:17:01 -0600

    add styling for event elements

--- src/app/components/events/EventBox.jsx
+++ src/app/components/events/EventBox.jsx
@@ -1,9 +1,24 @@
-import Event from '@/components/events/Events';
 
-function EventBox() {
+function EventBox({events}) {
+  console.log(events)
   return (
     <div>
-      
+    {events.map((e) => {
+      return (
+        <div className="bg-secondary/60 rounded-[10px] p-4 xl:p-12 relative">
+          <div className="flex flex-col xl:flex-row justify-between h-[620px] xl:full gap-4">
+            {/* image */}
+            <div className="hidden xl-flex w-[400px]">
+            </div>
+              {/* event list */}
+              <div className="flex-1 bg-purple-400/10 h-[500px] flex flex-col">
+                {/* events mapping */}
+              </div>
+          </div>
+
+        </div>
+      )
+    })}
     </div>
   )
 }
