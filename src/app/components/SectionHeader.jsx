d6f7365238c69595e04383d33bc9ce49cd8b6e61
Montywilliams <3650@holbertonschool.com>
2023-11-06 15:28:18 -0600

    import fadeIn efffect and framer motion library and render inside section elelments

--- src/app/components/SectionHeader.jsx
+++ src/app/components/SectionHeader.jsx
@@ -1,9 +1,27 @@
+'use client'
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../../variants';
+
 const SectionHeader = ({ pretitle, title }) => {
   return (
     <header>
-        {/* Header helps with seo */}
-      <h3 className="pretitle text-center">{pretitle}</h3>
-      <h2 className="h2 text-center mb-8">{title}</h2>
+      {/* Header helps with seo */}
+      <motion.h3
+        variants={fadeIn('up', 0.2)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{ once: false, amount: 0.3}}
+        className="pretitle text-center">
+          {pretitle}
+        </motion.h3>
+        <motion.h2
+          variants={fadeIn('up', 0.4)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.3}}
+          className="h2 text-center mb-8">
+          {title}
+        </motion.h2>
     </header>
   )
 }
