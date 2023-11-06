0e0c46855d84d70e09f396aa065b83dc47a0e785
Montywilliams <3650@holbertonschool.com>
2023-11-06 15:37:46 -0600

    import dependencies

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,4 +1,9 @@
 'use client'
+import Image from 'next/image';
+import { MouseParallaxContainer, mouseParallaxChild } from 'react-parallax-mouse';
+import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../../variants';
 
 const Hero = () => {
   return (
