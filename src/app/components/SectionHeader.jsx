832620254002d98033c1bf28d518072cc8958a40
Montywilliams <3650@holbertonschool.com>
2023-11-06 15:16:07 -0600

    add styling

--- src/app/components/SectionHeader.jsx
+++ src/app/components/SectionHeader.jsx
@@ -2,8 +2,8 @@ const SectionHeader = ({ pretitle, title }) => {
   return (
     <header>
         {/* Header helps with seo */}
-      <h3>{pretitle}</h3>
-      <h2>{title}</h2>
+      <h3 className="pretitle text-center">{pretitle}</h3>
+      <h2 className="h2 text-center mb-8">{title}</h2>
     </header>
   )
 }
