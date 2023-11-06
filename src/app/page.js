72c56a9d75a798db54abb0467b616c227a43d844
Montywilliams <3650@holbertonschool.com>
2023-11-06 15:35:13 -0600

    add hero component to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Events from '@/components/events/Events';
+import Hero from '@/components/Hero';
 
 export default function Home() {
   
   return (
     <main>
+      < Hero />
       <Events />
       <div className="h-[4000px]"></div>
     </main>
