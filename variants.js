9e35da33ba14087db2c94aa0f9b83e45928c1d12
Montywilliams <3650@holbertonschool.com>
2023-11-06 10:50:52 -0600

    add 'show' property for reveling elements

--- variants.js
+++ variants.js
@@ -9,6 +9,18 @@ export const fadeIn = (direction, delay) => {
         delay: delay,
         ease: [0.25, 0.5, 0.3, 0.8],
       },
+    },
+    show: {
+      y: 0,
+      x: 0,
+      opacity: 1, 
+      transition: {
+        type: 'tween',
+        duration: 1.4,
+        delay: delay,
+        ease: [0.25, 0.25, 0.25, 0.75],
+
+      }
     }
   }
 }
