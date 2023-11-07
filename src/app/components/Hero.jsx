03a914d7ccdff876552be672b10e32002a9aa29d
Montywilliams <3650@holbertonschool.com>
2023-11-06 23:36:57 -0600

    add locationSequence arrray; add divs in section for hero image and text

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -5,9 +5,29 @@ import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../../variants';
 
+const locationSequence = [
+  'Los Angeles, USA',
+  3000,
+  'Rio de Janeiro, Brazil',
+  3000,
+  'Paris, France',
+  3000,
+  'Berlin, Germany',
+  3000,
+  'Athens, Greece',
+  3000,
+]
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='h-[80vh] bg-pink-200/10 xl:h-[850px]'>
+      <div className='container mx-auto'>
+         {/* text */}
+        <div>text</div>
+        {/* image */}
+        <div>text</div>
+      </div>      
+    </section>
   )
 }
 
