85178d5082d0ca89becda46cadfb769fc94d1c29
Montywilliams <3650@holbertonschool.com>
2023-11-07 00:22:59 -0600

    add motion component: set parallax factor for child

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -23,22 +23,28 @@ const Hero = () => {
     <section className='h-[80vh] bg-pink-200/10 xl:h-[850px]' id='home'>
       <div className='container mx-auto bg-yellow-200/10 h-full flex justify-center items-center xl:justify-start'>
          {/* text */}
-        <div className='bg-red-500/10 h-full flex flex-col justify-center items-center xl:items-start z-20 pt-12'>
+        <div className=' h-full flex flex-col justify-center items-center xl:items-start z-20 pt-12'>
           <MouseParallaxContainer
             globalFactorX={0.1}
             globalFactorY={0.2}
             resetOnLeave
             className='relative flex items-center h-[120px] xl:h-max xl:w-[840px]'
             >
-            <MouseParallaxChild className='relative'>
-              <div className='w-[300px] h-[101.37px] xl:w-[725px] xl:h-[244.97px]'>
+            <MouseParallaxChild factorX={0.2} factorY={0.4} className='relative'>
+              <motion.div
+                variants={fadeIn('up', 0.4)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{once: false, amount: 0.3}}
+                className='w-[300px] h-[101.37px] xl:w-[725px] xl:h-[244.97px]'
+                >
                 <Image
                   src={'/assets/hero/notes_quarter.svg'}
                   fill
                   alt=''
                   className='object-contain'
                 />
-              </div>
+              </motion.div>
             </MouseParallaxChild>
           </MouseParallaxContainer>
         </div>
