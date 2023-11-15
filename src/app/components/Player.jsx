5d743957f11849012a00fde99ad96320cc5d8cda
Montywilliams <3650@holbertonschool.com>
2023-11-15 16:29:27 -0600

    make render on client; import dependencies, will use AudioPlayer library

--- src/app/components/Player.jsx
+++ src/app/components/Player.jsx
@@ -1,7 +1,21 @@
+'use client';
+
+import Image from 'next/image';
+import { AudionPlayer } from 'react-audio-play';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
 
 const Player = () => {
   return (
-    <div>Player</div>
+    <div>
+      <div className="container mx-auto">
+        {/* text and avatar img */}
+        text and avatar img
+        {/* player */}
+        <AudionPlayer />
+        
+      </div>
+    </div>
   )
 }
 
