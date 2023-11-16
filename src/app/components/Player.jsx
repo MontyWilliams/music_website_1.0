2ca9b0196e6918a8111eecc33c57c1f889789513
Montywilliams <3650@holbertonschool.com>
2023-11-16 09:54:04 -0600

    correct typo in AudioPlayer

--- src/app/components/Player.jsx
+++ src/app/components/Player.jsx
@@ -1,19 +1,26 @@
 'use client';
 
 import Image from 'next/image';
-import { AudionPlayer } from 'react-audio-play';
+import { AudioPlayer } from 'react-audio-play';
 import { motion } from 'framer-motion';
-import { fadeIn } from '../../variants';
+import { fadeIn } from '../../../variants';
 
 const Player = () => {
   return (
     <div>
-      <div className="container mx-auto">
+      <div className="container mx-auto  ">
         {/* text and avatar img */}
         text and avatar img
         {/* player */}
-        <AudionPlayer />
-        
+        <AudioPlayer
+          loop
+          preload='none'
+          
+          volume={48}
+          volumePlacement='top'
+          src='/assets/player/youngsmoke3.mp3'
+        />
+
       </div>
     </div>
   )
