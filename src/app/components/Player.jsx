2e51dca1adb46cec37b67472db66aaa28e61c04f
Montywilliams <3650@holbertonschool.com>
2023-12-18 09:06:27 -0600

    add motion div to player

--- src/app/components/Player.jsx
+++ src/app/components/Player.jsx
@@ -8,7 +8,12 @@ import '../globals.css'
 
 const Player = () => {
   return (
-    <div className='bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] flex items-center relative z-40'>
+    <motion.div
+      variants={fadeIn('up', 0.1)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.1}}
+    className='bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] flex items-center relative z-40'>
       <div className="container mx-auto flex flex-col justify-between items-center xl:flex-row ">
         {/* text and avatar img */}
         <div className='hidden w-[300px] xl:flex items-center gap-x-4'>
@@ -38,7 +43,7 @@ const Player = () => {
           />
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
