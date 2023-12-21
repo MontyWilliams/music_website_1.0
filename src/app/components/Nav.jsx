901e2c0468010b64c02ba1fc8820b6ca9ee7c2d4
Montywilliams <3650@holbertonschool.com>
2023-12-21 04:46:05 -0600

    create link array of navigatable areas; map through array and render

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,6 +1,31 @@
+import { Link } from "react-scroll"
+
+const links = [
+  {
+    path: 'home',
+    name: 'Home'
+  },
+  {
+    path: 'tours',
+    name: 'Tours'
+  },
+  {
+    path: 'discography',
+    name: 'Discography'
+  },
+  {
+    path: 'contact',
+    name: 'Contact'
+  },
+] 
+
 const Nav = ({containerStyles, linkStyles}) => {
   return (
-    <nav>Nav</nav>
+    <nav>
+      {links.map((link, index) => {
+        return <Link to={link.path}>{link.name}</Link>
+      })}
+    </nav>
   )
 }
 
