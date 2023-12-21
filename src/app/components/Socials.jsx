9e633b65b24dc6ea4780610b0dbf334b2ba8b7c8
Montywilliams <3650@holbertonschool.com>
2023-12-21 06:47:14 -0600

    create Socials comp; import icon deps; create array of social icons

---
+++ src/app/components/Socials.jsx
@@ -0,0 +1,26 @@
+import Link from 'nex/link';
+import { RiYoutubeFill, RiInstagramFill, RiSpotifyFill } from 'react-icons/ri';
+
+const socials= [
+  {
+    path: '#',
+    icon: <RiYoutubeFill />
+  },
+  {
+    path: '#',
+    icon: <RiInstagramFill />
+  },
+  {
+    path: '#',
+    icon: <RiSpotifyFill />
+  },
+]
+const Socials = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Socials
