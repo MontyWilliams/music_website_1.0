d3771bfb62e072badd88a851e0c41e7c56b408aa
Montywilliams <3650@holbertonschool.com>
2023-12-21 04:36:50 -0600

    style navbar; import NavMobile component and render into navbar

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,6 +3,7 @@
 import {useEffect, useState} from 'react'
 import Image from 'next/image';
 import Link from 'next/link'
+import NavMobile from './NavMobile';
 
 import {motion} from 'framer-motion'
 import { fadeIn } from '../../../variants'
@@ -24,7 +25,7 @@ const Header = () => {
     <header className={`fixed z-50 w-full ${
       active ? 'bg-[#030315] py-6 ' : 'bg-transparent py-8'
     }`}>
-      <div className="containe mx-auto flex flex-col xl:flex-row">
+      <div className="containe mx-auto flex flex-col xl:flex-row items-center justify-between">
         {/* logo */}
         <Link href={'#'} className='relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0'>
           <Image
@@ -36,7 +37,7 @@ const Header = () => {
         {/* nav */}
         <nav>nav</nav>
         {/* nav mobile */}
-        <nav>nav mobile</nav>
+        <NavMobile />
         {/* menu btn */}
         <div>menu btn</div>
         {/* socials */}
