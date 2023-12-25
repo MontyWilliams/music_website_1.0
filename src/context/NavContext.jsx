1db6d8058b477fcc4dc5b33ed91ae514b4be9a2a
Montywilliams <3650@holbertonschool.com>
2023-12-25 10:06:32 -0600

    will be using createContext for simple page state handling; really more for like a front end design

---
+++ src/context/NavContext.jsx
@@ -0,0 +1,13 @@
+import React, {createContext, useState } from 'react'
+
+export const NavContext = createContext();
+
+function NavContext() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavContext
