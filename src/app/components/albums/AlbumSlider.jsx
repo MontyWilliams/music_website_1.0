5cf75d6da69f2c5096388fdb3205f3229fcf085f
Montywilliams <3650@holbertonschool.com>
2024-01-01 13:59:36 -0600

    update styling for swiper

--- src/app/components/albums/AlbumSlider.jsx
+++ src/app/components/albums/AlbumSlider.jsx
@@ -30,9 +30,26 @@ function AlbumSlider() {
   return (
     <div>
        {/* top slider  */}
-       <Swiper>
+       <Swiper >
         {albums.map((e) => {
-          console.log(e)
+          return (
+            <SwiperSlide key={e.id} className='mb-12'>
+              <div className='w-full bg-Secondary/80 rounded-[10px] flex flex-col xl:flex-row items-center p-6 gap-x-12 '>
+              {/* img */}
+                <div className="hidden xl:flex w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] relative cursor-pointer rounded-[10px] overflow-hidden">
+                  <Image
+                    src={e.img}
+                    fill
+                    priority
+                    alt=''
+                    className='object-contain'
+                  />
+                </div>
+                {/* track container */}
+                <div>track container</div>
+              </div>
+            </SwiperSlide>
+          )
         })}
        </Swiper>
        {/* thumb slider */}
