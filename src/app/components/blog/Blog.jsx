1796d2cf9110625095526976c4f311dd92a9ffe6
Montywilliams <3650@holbertonschool.com>
2024-01-02 15:38:09 -0600

    add section title and PostList

--- src/app/components/blog/Blog.jsx
+++ src/app/components/blog/Blog.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import SectionHeader from "../SectionHeader"
+import PostList from "./PostList"
+import Posts from '../../../../_data/db.json'
+
 
 function Blog() {
+  const posts = Posts.posts
+
   return (
-    <div>
-      
-    </div>
+    <section className='section' id='blog'>
+      <div className="container mx-auto">
+        <SectionHeader pretitle='Our Blog' title='Latest News' />
+        {/* post list */}
+        <PostList  posts={posts} />
+      </div>
+    </section>
   )
 }
 
