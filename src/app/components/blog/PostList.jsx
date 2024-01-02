d11834ced26c38f112d2db178039752c2f3032bd
Montywilliams <3650@holbertonschool.com>
2024-01-02 15:46:37 -0600

    render first 3 posts in mapping

--- src/app/components/blog/PostList.jsx
+++ src/app/components/blog/PostList.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client'
 
-function PostList() {
+import Link from 'next/link';
+import { BsArrowRight } from 'react-icons/bs';
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../../../variants'
+
+function PostList({posts}) {
+  // get first 3 posts
+  const firstThreePosts = posts.slice(0, 3);
+
   return (
     <div>
-      
+      <div>
+        {firstThreePosts.map(post => {
+          // destructure posts
+          const {id, date, title, description } = post;
+          return (
+            <div className="">
+              <div>{date}</div>
+              <div>{title}</div>
+              <div>{description}</div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
