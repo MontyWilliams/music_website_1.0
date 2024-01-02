61ef80dc4904094c5ee67f562675fb06e264c244
Montywilliams <3650@holbertonschool.com>
2024-01-01 21:27:34 -0600

    style up album slider

--- src/app/components/albums/AlbumSlider.jsx
+++ src/app/components/albums/AlbumSlider.jsx
@@ -34,7 +34,7 @@ function AlbumSlider() {
         {albums.map((e) => {
           return (
             <SwiperSlide key={e.id} className='mb-12'>
-              <div className='w-full bg-Secondary/80 rounded-[10px] flex flex-col xl:flex-row items-center p-6 gap-x-12 '>
+              <div className='w-full bg-Secondary/80 rounded-[10px] flex flex-col xl:flex-row items-center p-6 gap-x-12 overflow-y-scroll scrollbar-thin scrollbar-thumb-accent scrollbar-track-white/10'>
               {/* img */}
                 <div className="hidden xl:flex w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] relative cursor-pointer rounded-[10px] overflow-hidden">
                   <Image
@@ -46,10 +46,35 @@ function AlbumSlider() {
                   />
                 </div>
                 {/* track container */}
-                <div>
-                  <div>
+                <div className='flex flex-1 w-full h-[500px]'>
+                  <div className='flex-1 flex flex-col xl:px-12'>
                     {e.tracks?.map((track, index) => {
-                      return <div key={index}>{track.name}</div>
+                      return (
+                        <div  className="flex flex-1 w-full h-[500px]" key={index}>
+                        {/* track name */}
+                          <div className='flex flex-1 items-center gap-x-2 capitalize font-semibold xl:font-extrabold'>
+                            <div className='text-accent text-sm xl:text-lg'>0{index + 1}.</div>
+                            <div className='text-sm xl:text-base'>{track.name}</div>
+                          </div>
+                          {/* player */}
+                          <div>
+                            <AudioPlayer
+                              style={{
+                                background: 'transparent',
+                                boxShadow: 'none',
+                                width: '100%',
+                                }}
+                              src={track.src}
+                              loop
+                              preload='none'
+                              color='#fff'
+                              volume={40}
+                              volumePlacement='bottom'
+                              className='album-player'
+                              />
+                          </div>
+                        </div>
+                      )
                     })}
                   </div>
                 </div>
