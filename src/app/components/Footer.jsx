ec0c244a2e54f16ebc6a4ae5718c24e07e1f755e
Montywilliams <3650@holbertonschool.com>
2024-01-03 10:13:59 -0600

    render Nav and Social comps pasing in styling for each (pre built within function)

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -10,9 +10,30 @@ import { fadeIn } from '../../../variants';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className='bg-accent section'>
+      <div className="container mx-auto">
+        {/* email link */}
+        <div>
+          <Link href='#'>
+            <h2>management@thebadjokesband.com</h2>
+          </Link>
+          {/* nav */}
+          <div>
+            <Nav
+              containerStyles='flex flex-col xl:flex-row justify-center items-center gap-y-4 xl:gap-x-8 text-sm uppercase font-semibold'
+              linkStyles='hover:text-primary/80 transition-all'
+            />
+          </div>
+          {/* socials */}
+          <div>
+            <Socials
+              containerStyles='flex text-[24px] gap-x-8'
+              iconStyles='hover:text-primary/80 transition-all justify-center'
+            />
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
