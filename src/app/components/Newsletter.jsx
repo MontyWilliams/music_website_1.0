671ce31a0bd1fbfe644c3685c2401eaa7e82b9aa
Montywilliams <3650@holbertonschool.com>
2024-01-03 00:21:52 -0600

    add bg and input to join newsletter

--- src/app/components/Newsletter.jsx
+++ src/app/components/Newsletter.jsx
@@ -6,8 +6,26 @@ import { fadeIn } from '../../../variants';
 
 function Newsletter() {
   return (
-    <section className="bg-newsletter bg-fixed h-[480px]">
-      Newsletter
+    <section
+      className="bg-newsletter bg-fixed h-[480px] bg-no-repeat bg-cover w-full bg-center"
+      id='contact'  
+    >
+      <div className="container mx-auto h-full">
+        <div>
+          {/*  text */}
+          <SectionHeader
+            pretitle='Get in touch'
+            title='Sign up to our newsletter'
+          />
+          {/* input */}
+          <div className="relative flex items-center w-full max-w-xl">
+            <input type="text" placeholder="Email address" className="w-full"/>
+            <button type='submit' className="bg-accent rounded-sm">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
