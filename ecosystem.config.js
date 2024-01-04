415d0c1581f7bca310b3b12ab73c5a8b1a5602e8
Montywilliams <3650@holbertonschool.com>
2024-01-04 07:30:43 -0600

    update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       repo : 'https://github.com/MontyWilliams/music_website_1.0.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
         'ssh_options': 'ForwardAgent=yes'
     }
