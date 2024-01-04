32ebbf222b4c2082b07eba97edbf82575beb222f
Montywilliams <3650@holbertonschool.com>
2024-01-04 07:26:53 -0600

    add config

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+  }], 
+  deploy : {
+    production : {
+        key: 'key.pem',
+      user : 'ubuntu',
+      host : '3.129.69.112',
+      ref  : 'origin/master',
+      repo : 'https://github.com/MontyWilliams/music_website_1.0.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+        'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
